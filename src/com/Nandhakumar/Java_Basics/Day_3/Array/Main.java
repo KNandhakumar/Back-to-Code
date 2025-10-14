b0426e87a7bfed3504f1a4ba0b74681121e55711
Nandhakumar <nknandhakumar64@gmail.com>
2025-10-14 15:32:21 +0530

    Array basic syntax

---
+++ src/com/Nandhakumar/Java_Basics/Day_3/Array/Main.java
@@ -0,0 +1,20 @@
+package com.Nandhakumar.Java_Basics.Day_3.Array;
+
+public class Main {
+    public static void main(String[] args) {
+        // Array syntax
+        int[] num = new int[4];
+        // if we already know, what type of values, we can do like this
+        String[] name = {"Nk","Mami"};
+        num[0] = 1;
+        num[1] = 2;
+        num[2] = 3;
+        num[3] = 4;
+        System.out.println(num[0]);
+        System.out.println(num[1]);
+        System.out.println(num[2]);
+        System.out.println(num[3]);
+        System.out.println(name[0]);
+        System.out.println(name[1]);
+    }
+}
