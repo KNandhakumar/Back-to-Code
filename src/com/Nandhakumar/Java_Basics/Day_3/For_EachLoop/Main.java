18226a8082e6f5f32eb45a9b8d53d61376c19260
Nandhakumar <nknandhakumar64@gmail.com>
2025-10-15 12:24:30 +0530

    For each loop

---
+++ src/com/Nandhakumar/Java_Basics/Day_3/For_EachLoop/Main.java
@@ -0,0 +1,10 @@
+package com.Nandhakumar.Java_Basics.Day_3.For_EachLoop;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        for (int num : arr){
+            System.out.print(num + " ");
+        }
+    }
+}
