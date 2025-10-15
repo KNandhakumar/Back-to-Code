deeadff47909ff836150b9189ebe8e54040a93c7
Nandhakumar <nknandhakumar64@gmail.com>
2025-10-15 19:19:24 +0530

    Super keyword practice question

---
+++ src/com/Nandhakumar/Oops/SuperKeyword/Excersize/Main.java
@@ -0,0 +1,24 @@
+package com.Nandhakumar.Oops.SuperKeyword.Excersize;
+
+class Person{
+    String name;
+    Person(String name){
+        this.name = name;
+    }
+}
+
+class Employee extends Person{
+    int employeeId;
+
+    Employee(String name){
+        super(name);
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Nandhakumar");
+        System.out.println(employee.name);
+    }
+}
