c7bcb82b4ea57a2587f0bb4cf82c527c3ade091e
Nandhakumar <nknandhakumar64@gmail.com>
2025-10-16 20:39:20 +0530

    final keyword

---
+++ src/com/Nandhakumar/Oops/Final/Main.java
@@ -0,0 +1,11 @@
+package com.Nandhakumar.Oops.Final;
+
+public class Main {
+    public static void main(String[] args) {
+        // Final keyword in java is used to restrict modifications. it can be applied to
+        // variables, methods and classes.
+        // final variable (constant) -> Once assigned, its value cannot be changed.
+        // final method -> cannot be override.
+        // final class -> cannot be extends or inherit (no sub classes allowed)
+    }
+}
