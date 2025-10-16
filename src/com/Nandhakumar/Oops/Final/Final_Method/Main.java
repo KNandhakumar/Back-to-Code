518df9292bbb22a6d114b7964cec61f3eb55c28a
Nandhakumar <nknandhakumar64@gmail.com>
2025-10-16 20:51:14 +0530

    final keyword : final method

---
+++ src/com/Nandhakumar/Oops/Final/Final_Method/Main.java
@@ -0,0 +1,19 @@
+package com.Nandhakumar.Oops.Final.Final_Method;
+
+class Animal{
+    final void display(){
+        System.out.println("this is final method for Animal class");
+    }
+}
+
+//class Dog extends Animal{
+//    void display(){
+//        System.out.println("cannot be override in final method");
+//    }
+//}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
