04f341acbbfc963d3d1667ab262c543fec7b40b2
Nandhakumar <nknandhakumar64@gmail.com>
2025-10-16 17:22:15 +0530

    Access Modifiers : all types explanation very easy

---
+++ src/com/Nandhakumar/Oops/AccessModifiers/AccessModifiers.java
@@ -0,0 +1,12 @@
+package com.Nandhakumar.Oops.AccessModifiers;
+
+public class AccessModifiers {
+    public static void main(String[] args) {
+        // Access modifiers control the visibility of variables, methods, and classes.
+        // Public -> Accessible everywhere
+        // Private -> Only within the same class
+        // Protected -> Only within the same package and sub classes
+        // Default -> Only within the same package
+        // USage : Helps in data hiding, encapsulation, and security in java
+    }
+}
