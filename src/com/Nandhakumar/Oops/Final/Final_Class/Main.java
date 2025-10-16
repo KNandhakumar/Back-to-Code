098c4bf4530193c415b33f6ac0f0ac57ff88226b
Nandhakumar <nknandhakumar64@gmail.com>
2025-10-16 20:54:46 +0530

    final keyword : final class

---
+++ src/com/Nandhakumar/Oops/Final/Final_Class/Main.java
@@ -0,0 +1,16 @@
+package com.Nandhakumar.Oops.Final.Final_Class;
+
+// final class not accept for inheritance (extends)
+final class Shape{
+
+}
+
+//class Diamond extends Shape{
+//
+//}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
