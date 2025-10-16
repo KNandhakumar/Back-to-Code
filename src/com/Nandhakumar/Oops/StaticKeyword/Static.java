fe7281b4fd6e72ff954d01775e0284371e4e89c1
Nandhakumar <nknandhakumar64@gmail.com>
2025-10-16 19:29:06 +0530

    Static keyword

---
+++ src/com/Nandhakumar/Oops/StaticKeyword/Static.java
@@ -0,0 +1,13 @@
+package com.Nandhakumar.Oops.StaticKeyword;
+
+public class Static {
+    public static void main(String[] args) {
+        // In java, static keyword means a variable, method or block belongs to the class rather than
+        // any specific instance(object)
+        // static variable -> shared across all objects of the class.
+        // static method -> can be called without creating of the class.
+        // static block -> runs once when the class is loaded.
+        // static class (nested) -> a class inside another class that dosent need an instance of the outer class.
+        // usage -> saves memory, useful for utility methods.
+    }
+}
