90694d437aa278f4e0b4007ea46ae6c9840c8562
Nandhakumar <nknandhakumar64@gmail.com>
2025-10-17 20:14:08 +0530

    Finally keyword

---
+++ src/com/Nandhakumar/Oops/Exception_Handling/Finally/Main.java
@@ -0,0 +1,16 @@
+package com.Nandhakumar.Oops.Exception_Handling.Finally;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int a = 10/0;
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        // finally is always run whether exception occur or not
+        finally {
+            System.out.println("always run im finally");
+        }
+    }
+}
